d30c4c62d56abdb58cd5903d74a68f24b55ae1d3
Ben Coleman <benjamin.coleman@me.com>
2024-03-01 22:09:12 +0000

    Fixed options conflict issue

--- config/default.nix
+++ config/default.nix
@@ -11,51 +11,58 @@
     ./treesitter.nix
     ./harpoon.nix
   ];
-  colorschemes.rose-pine = {
-    enable = true;
-    style = "moon";
-  };
 
-  clipboard.register = "unnamedplus";
-
-  globals = {
-    mapleader = " ";
-    undofile = true;
-  };
-  
-  localOptions = {
-    relativenumber = true;
-    mouse = "a";
-    hlsearch = false; # Don't leave searches highlighted 
-    breakindent = true;
-  };
-
-  plugins.comment-nvim.enable = true; # use gc<Motion> to comment lines
-  plugins.indent-blankline.enable = true; # show indent lines
-
-  plugins.surround.enable = true;
-
-  extraPlugins = with pkgs.vimPlugins; [
-    vim-sleuth
-  ];
-
-  # Yank on Highlight
-  autoGroups = {
-    YankHighlight = {
-      clear = true;
+  config = {
+    globals = {
+      mapleader = " ";
     };
-  };
+  
+    colorschemes.rose-pine = {
+      enable = true;
+      style = "moon";
+    };
 
-  autoCmd = [
-  {
-    event = "TextYankPost";
-    callback = { __raw = ''
-    function()
-      vim.highlight.on_yank()
-    end
-    '';};
-    group = "YankHighlight";
-    pattern = "*";
-  }
-  ];
+    clipboard.register = "unnamedplus";
+
+    options = {
+      relativenumber = true;
+      undofile = true;
+      breakindent = true;
+      mouse = "a";
+      hlsearch = false; # Don't leave searches highlighted 
+      showmode = false;
+      ignorecase = true;
+      smartcase = true;
+      signcolumn = "yes";
+    };
+
+    autoCmd = [
+      {
+        event = "TextYankPost";
+        callback = { __raw = ''
+        function()
+          vim.highlight.on_yank()
+        end
+        '';};
+        group = "YankHighlight";
+        pattern = "*";
+      }
+    ];
+
+    # Yank on Highlight
+    autoGroups = {
+      YankHighlight = {
+        clear = true;
+      };
+    };
+
+    extraPlugins = with pkgs.vimPlugins; [
+      vim-sleuth
+    ];
+
+    plugins.comment-nvim.enable = true; # use gc<Motion> to comment lines
+    plugins.indent-blankline.enable = true; # show indent lines
+
+    plugins.surround.enable = true;
+  };
 }
