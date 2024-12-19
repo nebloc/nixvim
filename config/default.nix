7bf498497ed0d1ad1ecff0d180e1f00412cd2078
Ben Coleman <benjamin.coleman@me.com>
2024-12-19 17:00:45 +0000

    Breaking metals out in to own config file

--- config/default.nix
+++ config/default.nix
@@ -10,6 +10,7 @@
     ./telescope.nix
     ./treesitter.nix
     ./harpoon.nix
+    ./metals.nix
   ];
 
 
@@ -18,9 +19,10 @@
       mapleader = " ";
     };
   
-  plugins.dap = {
-  enable = true;
-  };
+    plugins.dap = {
+      enable = true;
+    };
+
     colorschemes.rose-pine = {
       enable = true;
       style = "moon";
@@ -51,43 +53,6 @@
         group = "YankHighlight";
         pattern = "*";
       }
-      {
-        event = "FileType";
-        pattern = [ "scala" "sbt" "java" ];
-        callback = {
-          __raw = /*lua */''
-            function()
-              local opts = { noremap = true, silent = true }
-              local api = vim.api
-              local cmd = vim.cmd
-              local metals = require("metals")
-              local metals_config = metals.bare_config()
-
-              local capabilities = vim.lsp.protocol.make_client_capabilities()
-              local cmp_nvim_lsp = require("cmp_nvim_lsp")
-              metals_config.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-
-              -- metals_config.on_attach = function(client, bufnr)
-              --   metals.setup_dap()
-              -- end
-
-              metals_config.init_options = {
-                statusBarProvider = "lualine",
-                icons = "unicode",
-                disableColorOutput = false,
-              }
-
-              metals_config.settings = {
-                showImplicitArguments = true,
-                showImplicitConversionsAndClasses = true,
-                showInferredType = true,
-              }
-              require("metals").initialize_or_attach(metals_config)
-            end
-          '';
-        };
-        group = "nvim-metals";
-      }
     ];
 
     # Yank on Highlight
@@ -95,13 +60,9 @@
       YankHighlight = {
         clear = true;
       };
-      nvim-metals = {
-        clear = true;
-      };
     };
 
     extraPlugins = with pkgs.vimPlugins; [
-      nvim-metals
       vim-sleuth
     ];
 
--- config/metals.nix
+++ config/metals.nix
@@ -1,43 +1,52 @@
+{pkgs, ...}:
 {
-  programs.nixvim = {
-    autoCmd = [
-      {
-        event = "FileType";
-        pattern = [ "scala" "sbt" "java" ];
-        callback = {
-          __raw = /*lua*/''
-            function()
-              local opts = { noremap = true, silent = true }
-              local api = vim.api
-              local cmd = vim.cmd
-              local metals = require("metals")
-              local metals_config = metals.bare_config()
+  extraPlugins = with pkgs.vimPlugins; [
+    nvim-metals
+  ];
 
-              local capabilities = vim.lsp.protocol.make_client_capabilities()
-              local cmp_nvim_lsp = require("cmp_nvim_lsp")
-              metals_config.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+  autoCmd = [
+    {
+      event = "FileType";
+      pattern = [ "scala" "sbt" "java" ];
+      callback = {
+        __raw = /*lua*/''
+          function()
+            local opts = { noremap = true, silent = true }
+            local api = vim.api
+            local cmd = vim.cmd
+            local metals = require("metals")
+            local metals_config = metals.bare_config()
 
-              -- metals_config.on_attach = function(client, bufnr)
-              --   metals.setup_dap()
-              -- end
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            metals_config.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
-              metals_config.init_options = {
-                statusBarProvider = "lualine",
-                icons = "unicode",
-                disableColorOutput = false,
-              }
+            -- metals_config.on_attach = function(client, bufnr)
+            --   metals.setup_dap()
+            -- end
 
-              metals_config.settings = {
-                showImplicitArguments = true,
-                showImplicitConversionsAndClasses = true,
-                showInferredType = true,
-              }
-              require("metals").initialize_or_attach(metals_config)
-            end
-        '';
-        };
-        group = "nvim-metals";
-      }
-    ];
+            metals_config.init_options = {
+              statusBarProvider = "lualine",
+              icons = "unicode",
+              disableColorOutput = false,
+            }
+
+            metals_config.settings = {
+              showImplicitArguments = true,
+              showImplicitConversionsAndClasses = true,
+              showInferredType = true,
+            }
+            require("metals").initialize_or_attach(metals_config)
+          end
+      '';
+      };
+      group = "nvim-metals";
+    }
+  ];
+
+  autoGroups = {
+    nvim-metals = {
+      clear = true;
+    };
   };
 }
